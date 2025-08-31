88548a5085f2ea3978e9a963a61deff3199bbef3
Filip Zuziak <115494432+ZuziakF@users.noreply.github.com>
2025-08-31 20:15:15 +0200

    Tetris
    Gra w tetris napisana za pomocą biblioteki raylib oraz kursu udostępnionego na platformie YouTube

---
+++ Tetris/Tetris/block.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <map>
+#include "position.h"
+#include "colors.h"
+using namespace std;
+class Block
+{
+public:
+	Block();
+	void Draw(int offsetX,int offsetY);
+	void Move(int row, int columns);
+	vector<Position> GetCellPositions();
+	void Rotate();
+	void UndoRotation();
+	int id;
+	map<int, vector<Position>> cells;
+private:
+	int cellSize;
+	int rotationState;
+	vector<Color> colors;
+	int rowOffset;
+	int colOffset;
+};
